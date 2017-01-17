ede5d5562f8722390cf7e1a9e6554c26c695519e
Chris <cturner179@gmail.com>
2017-01-16 19:33:41 -0500

    Site
    Add files

---
+++ jscode.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#aboutsub").show();
+    $("#aboutheader").click(function(){
+        $("#aboutsub").slideToggle();
+    });
+    $("#linksheader").click(function(){
+        $("#linkssub").slideToggle();
+    });
+    $("#projectsheader").click(function(){
+        $("#projectssub").slideToggle();
+    });
+    $("#skillsheader").click(function(){
+        $("#skillssub").slideToggle();
+    });
+    $("#experienceheader").click(function(){
+        $("#experiencesub").slideToggle();
+    });
+    
+
+});
