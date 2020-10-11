3a09ae9038cb1653a56516bce4c04f6312814fed
Chris <chrispturner@Protonmail.com>
2020-10-11 18:37:52 -0400

    updates

--- resume.js
+++ resume.js
@@ -1,20 +1,7 @@
 $(document).ready(function(){
-    $("#aboutsub").show();
-    $("#aboutheader").click(function(){
-        $("#aboutsub").slideToggle();
-    });
-    $("#linksheader").click(function(){
-        $("#linkssub").slideToggle();
-    });
-    $("#projectsheader").click(function(){
-        $("#projectssub").slideToggle();
-    });
-    $("#skillsheader").click(function(){
-        $("#skillssub").slideToggle();
-    });
-    $("#experienceheader").click(function(){
-        $("#experiencesub").slideToggle();
-    });
+    $("#github").attr('title', 'GitHub');
+    $("#linkedin").attr('title', 'LinkedIn');
+    $("#resume").attr('title', 'Resume');
     
 
 });
